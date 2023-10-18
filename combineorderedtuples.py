a97c5e0ff6ce71d7eccecf44e108cad2ded3ec26
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-10-18 13:45:55 -0400

    Renamed virusconfigurations to virusdata

--- combineorderedtuples.py
+++ combineorderedtuples.py
@@ -1,12 +1,12 @@
 import sympy as sp
-from virusdata import virusconfigurations
+from virusdata import virusdata
 from matrixgroups import icosahedralgroup, a4group, d10group, d6group
 
 if __name__ == "__main__":
     sp.init_printing()
-    sp.pprint(virusconfigurations.configs[12])
+    sp.pprint(virusdata.configs[12])
     sp.pprint(a4group.centralizer())
     sp.pprint(d10group.centralizer())
     sp.pprint(d6group.centralizer())
 
-    sp.pprint(icosahedralgroup.orbitOfVector(virusconfigurations.configs[12][1]))
+    sp.pprint(icosahedralgroup.orbitOfVector(virusdata.configs[12][1]))
