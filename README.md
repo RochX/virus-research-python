247544e300851705da8f9ad9962603483cf9f463
Xavier Silva <Xavier.Silva20@kzoo.edu>
2024-03-30 12:43:33 -0400

    Start implementing interacting with Jigwe from Python

--- README.md
+++ README.md
@@ -13,3 +13,20 @@ The main script is `combineorderedtuples.py`.
 The two helper scripts are `permutetestcases.py` and `picklestocsv.py`.
 
 Running any of them with the `-h` or `--help` flag with give more information on how to use them.
+
+## SSH Getter
+This code has been running remotely on the Kalamazoo College supercomputing cluster Jigwe (pronouced 'Cheekua', Potawatomi for Thunderbird).
+In order to retrieve results from Jigwe in a user-friendly fashion, the `ssh_getter.py` code allows one to get results from Jigwe.
+However, it order to use it, one must set up the SSH configuration file.
+This file is typically located in `~/.ssh/config`.
+If this file does not exist then create it (e.g. `touch ~/.ssh/config`).
+Then add the following lines to the SSH configuration file.
+```
+Host jigwe.kzoo.edu
+    AddKeysToAgent yes
+    IdentityFile ~/.ssh/MY_PRIVATE_KEY
+    User MY_USER
+```
+After doing this the program should work as expected.
+Note that one can manually verify that this worked by running `ssh jigwe.kzoo.edu`.
+If you have set up the configuration file correctly, this will now have the same effect as running `ssh USER@jigwe.kzoo.edu -i ~/.ssh/MY_PRIVATE_KEY`.
---
+++ ssh_getter/ssh_getter.py
@@ -0,0 +1,46 @@
+import fabric
+import sys
+import os
+# NOTE: requires ~/.ssh/config to be set up...
+# Host jigwe.kzoo.edu
+#     AddKeysToAgent yes
+#     IdentityFile ~/.ssh/xavier_macbook_jigwe
+#     User xavier
+
+
+def verify_ssh_config_is_setup(hostname, ssh_dir="~/.ssh/", ssh_config_filename="config"):
+    """
+    Returns True/False if hostname is within the ssh config file.
+    Defaults to searching for '~/.ssh/config'.
+    Raises a FileNotFoundError if the ssh config file does not exist.
+    """
+    ssh_config_path = os.path.join(os.path.expanduser(ssh_dir), ssh_config_filename)
+
+    if not os.path.exists(ssh_config_path):
+        raise FileNotFoundError(f"The SSH config file at path {ssh_config_path} does not exist.")
+
+    with open(ssh_config_path, 'r') as ssh_config:
+        ssh_config_contents = ssh_config.read()
+
+        return hostname in ssh_config_contents
+
+
+if __name__ == "__main__":
+    jigwe_hostname = "jigwe.kzoo.edu"
+
+    try:
+        ssh_config_setup = verify_ssh_config_is_setup(jigwe_hostname)
+    except FileNotFoundError as e:
+        ssh_config_setup = False
+        print(e)
+
+    if not ssh_config_setup:
+        print(f"Host '{jigwe_hostname}' is not set up within the SSH configuration file.\n"
+              f"See README for details on how to set it up.")
+        sys.exit(0)
+
+    with fabric.Connection(host=jigwe_hostname) as jigwe:
+        res = jigwe.run('cat cmds.txt')
+        print("\n\n\n")
+        print(res)
+
